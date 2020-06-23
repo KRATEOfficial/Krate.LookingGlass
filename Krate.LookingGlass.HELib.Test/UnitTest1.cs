8ebe0679ea0b93c78f681b48827ff60d42553d71
Michael Thoreson <m.thoreson@krate.com>
2020-06-22 22:19:31 -0600

    Repo Initialization

---
+++ Krate.LookingGlass.HELib.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Krate.LookingGlass.HELib.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
